7db9985f71ccab44049b605678b136a7a03dd9ac
dcollis92 <davidphilipcollis@gmail.com>
2022-05-31 14:46:48 -0500

    hook error correction

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -14,7 +14,7 @@ const Map: React.FC<MapProps> = ({
   children,
   style,
   ...options
-}) => {\
+}) => {
   // Map Hooks
   const ref = React.useRef<HTMLDivElement>(null);
   const [map, setMap] = React.useState<google.maps.Map>();
