5d77609b960863fcfe99d26bab411814cfe8da93
dcollis92 <davidphilipcollis@gmail.com>
2022-05-31 14:45:11 -0500

    add additional app state components

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -14,9 +14,11 @@ const Map: React.FC<MapProps> = ({
   children,
   style,
   ...options
-}) => {
+}) => {\
+  // Map Hooks
   const ref = React.useRef<HTMLDivElement>(null);
   const [map, setMap] = React.useState<google.maps.Map>();
+
 
   React.useEffect(() => {
     if (ref.current && !map) {
