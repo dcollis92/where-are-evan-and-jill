483ac969b1abc75ccd10b8b16022289d81f8db08
Tim Crisp <timothycrisp@gmail.com>
2022-05-31 10:37:16 -0500

    create marker component

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,17 +1,52 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-const Map: React.FC<{}> = () => {
+interface MapProps extends google.maps.MapOptions {
+  style: { [key: string]: string };
+  onClick?: (e: google.maps.MapMouseEvent) => void;
+  onIdle?: (map: google.maps.Map) => void;
+  children?: React.ReactNode;
+}
+
+const Map: React.FC<MapProps> = ({
+  onClick,
+  onIdle,
+  children,
+  style,
+  ...options
+}) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const [map, setMap] = React.useState<google.maps.Map>();
-  
+
   React.useEffect(() => {
     if (ref.current && !map) {
       setMap(new window.google.maps.Map(ref.current, {}));
     }
   }, [ref, map]);
 
-  return <div ref={ref} />
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options);
+    }
+  }, [map, options]);
+
+  React.useEffect(() => {
+    if (map) {
+      ["click", "idle"].forEach((eventName) =>
+        google.maps.event.clearListeners(map, eventName)
+      );
+
+      if (onClick) {
+        map.addListener("click", onClick);
+      }
+
+      if (onIdle) {
+        map.addListener("idle", () => onIdle(map));
+      }
+    }
+  }, [map, onClick, onIdle]);
+
+  return <div ref={ref} style={style} />;
 };
 
-export default Map
+export default Map;
